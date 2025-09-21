af5d9beb5e5766f62d6e8c461cd3e14b1683c96e
runesc <luisalfredoreyes98@gmail.com>
2025-09-20 20:53:54 -0600

    fix: Correct formatting of classifiers in setup.py

--- setup.py
+++ setup.py
@@ -51,8 +51,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules"
-
+        "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
